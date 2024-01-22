37c067ca0930fb9bb8008fc0114c9473c2a15c83
rtappeta3 <87485649+rtappeta3@users.noreply.github.com>
2024-01-22 17:49:46 -0500

    Add files via upload

--- herculesDisconnect.py
+++ herculesDisconnect.py
@@ -6,31 +6,55 @@ import filecmp
 import time
 from playsound import playsound
 
-#E
+
 
 def fileCopy(src, dst):
     try:
         shutil.copyfile(src, dst)
        
     except:
-        print("sadness")
 
 
 def alarm():
-    playsound('C:\Users\Owner\Downloads\imperial march.mp3')
+    playsound("imperial march trimmed.mp3")
     
+src = "longtermrun.csv"
+dst = "longtermrunBACKUP"
+step = 0
 
-src = "C:\Users\Owner\Documents\longtermrun.csv"
-dst = "C:\Users\Owner\Documents\longtermrunBackup.csv"
+def makeFile():
+    dst = "longtermrunBACKUP" + (str)step + ".csv"
+    while os.path.isfile(dst):
+        step++
+        dst = "longtermrunBACKUP" + (str)step + ".csv"
+    
+    with open(dst, 'w') as f: #create file when file doesn't exist
+            print("created file") 
+
+makeFile()
 
 while True:
     
     time.sleep(1)
+    if not os.path.isfile(dst): #if the file doesn't exist
+        makeFile()
+        
+    
+    if not filecmp.cmp(src, dst): #if files are NOT the same
+        if os.path.getsize(src) > os.path.getsize(dst): #if src is longer than dst
+            fileCopy(src, dst) #overrite so dst matches src
+            print("overwritten")
+        
+        else: #if 
+            makeFile()
 
-    if not filecmp.cmp(src, dst):
-        fileCopy(src, dst)
-
-    else: 
+    else: #if files are the same
+        print("alarm")
         alarm()
+        x = input("Press something after files have been made again")
+        #time.sleep(40)
+        
+        
+        
 
 
